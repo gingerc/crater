c5c05e0afdea211e96ceeb1f44cef51ddddac314
Ellie Lin <ellielin1014@gmail.com>
2019-07-20 09:28:20 -0400

    change value

--- public/sketch.js
+++ public/sketch.js
@@ -71,8 +71,7 @@ output.innerHTML = slider.value;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
-  if(slider.value!= " ") {rockSize = slider.value}
-  else {rockSize = "1"}
+  rockSize = slider.value;
 }
 
 function asteroidSelected(){
@@ -113,7 +112,7 @@ function goToSize(){
 
 
 function goToName(){
-  if (slider.value == " "){
+  if (slider.value == " " || slider.value == "1"){
     alert("Please choose the size of your impactor!");
     return false;
   } else{
