f89db58bc5525b0794fc5a9002a5fcc59e9f17ff
Ellie Lin <ellielin1014@gmail.com>
2019-07-19 23:41:48 -0400

    test values

--- public/sketch.js
+++ public/sketch.js
@@ -374,27 +374,27 @@ function handleGesure() {
          timeleft -= 1;
         if(timeleft <= 0){
             clearInterval(downloadTimer);
-            if (stationNum == "1" && (vel < 17 || vel > 100)){
+            if (stationNum === "1" && (vel < 17 || vel > 100)){
               $("#loading").css("display","none");
               $("#launchMessage").html("Oops you missed! <br> Try Again!");
               $(".launchPage").css("display", "inline-block");
             }
-            if (stationNum == "2" && (vel < 15 || vel > 28)){
+            if (stationNum === "2" && (vel < 15 || vel > 28)){
               $("#loading").css("display","none");
               $("#launchMessage").html("Oops you missed! <br> Try Again!");
               $(".launchPage").css("display", "inline-block");
             }
-            if (stationNum == "3" && (vel < 15 || vel > 28)){
+            if (stationNum === "3" && (vel < 15 || vel > 28)){
               $("#loading").css("display","none");
               $("#launchMessage").html("Oops you missed! <br> Try Again!");
               $(".launchPage").css("display", "inline-block");
             }
-            if (stationNum == "4" && (vel < 15 || vel > 28)){
+            if (stationNum === "4" && (vel < 15 || vel > 28)){
               $("#loading").css("display","none");
               $("#launchMessage").html("Oops you missed! <br> Try Again!");
               $(".launchPage").css("display", "inline-block");
             }
-            if (stationNum == "5" && (vel < 15 || vel > 28)){
+            if (stationNum === "5" && (vel < 15 || vel > 28)){
               $("#loading").css("display","none");
               $("#launchMessage").html("Oops you missed! <br> Try Again!");
               $(".launchPage").css("display", "inline-block");
